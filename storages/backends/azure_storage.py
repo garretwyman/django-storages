c1da7e3069505a8f3c6a7deda32834e0c01eb9dc
Garret Wyman <garretwyman@microsoft.com>
2024-06-06 10:41:27 -0400

    Patch to apply storage account managed identity changes
    updating

--- storages/backends/azure_storage.py
+++ storages/backends/azure_storage.py
@@ -22,6 +22,20 @@ from storages.utils import safe_join
 from storages.utils import setting
 from storages.utils import to_bytes
 
+try:
+    from django_guid import get_guid
+except ImportError:
+    def get_guid():
+        return None
+
+
+def _make_headers():
+    headers = {}
+    correlation_id = get_guid()
+    if correlation_id is not None:
+        headers["x-ms-client-request-id"] = correlation_id
+    return headers
+
 
 @deconstructible
 class AzureStorageFile(File):
@@ -45,7 +59,7 @@ class AzureStorageFile(File):
 
         if "r" in self._mode or "a" in self._mode:
             download_stream = self._storage.client.download_blob(
-                self._path, timeout=self._storage.timeout
+                self._path, timeout=self._storage.timeout, headers=_make_headers()
             )
             download_stream.readinto(file)
         if "r" in self._mode:
@@ -273,13 +287,15 @@ class AzureStorage(BaseStorage):
 
     def delete(self, name):
         try:
-            self.client.delete_blob(self._get_valid_path(name), timeout=self.timeout)
+            self.client.delete_blob(
+                self._get_valid_path(name), timeout=self.timeout, headers=_make_headers()
+            )
         except ResourceNotFoundError:
             pass
 
     def size(self, name):
         blob_client = self.client.get_blob_client(self._get_valid_path(name))
-        properties = blob_client.get_blob_properties(timeout=self.timeout)
+        properties = blob_client.get_blob_properties(timeout=self.timeout, headers=_make_headers())
         return properties.size
 
     def _save(self, name, content):
@@ -299,6 +315,7 @@ class AzureStorage(BaseStorage):
             max_concurrency=self.upload_max_conn,
             timeout=self.timeout,
             overwrite=self.overwrite_files,
+            headers=_make_headers(),
         )
         return cleaned_name
 
@@ -362,7 +379,7 @@ class AzureStorage(BaseStorage):
         USE_TZ is True, otherwise returns a naive datetime in the local timezone.
         """
         blob_client = self.client.get_blob_client(self._get_valid_path(name))
-        properties = blob_client.get_blob_properties(timeout=self.timeout)
+        properties = blob_client.get_blob_properties(timeout=self.timeout, headers=_make_headers())
         if not setting("USE_TZ", False):
             return timezone.make_naive(properties.last_modified)
 
@@ -384,7 +401,7 @@ class AzureStorage(BaseStorage):
         return [
             blob.name
             for blob in self.client.list_blobs(
-                name_starts_with=path, timeout=self.timeout
+                name_starts_with=path, timeout=self.timeout, headers=_make_headers()
             )
         ]
 
